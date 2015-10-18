d2b5f16649f7513d8df34da8de7a714f899282f7
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2015-10-18 18:58:32 -0400

    Implement doctest for q4

--- asst1/q4.py
+++ asst1/q4.py
@@ -1,7 +1,30 @@
 import math
 
 def factorize(n):
-    factors = [1]
+    """ Takes an integer N and returns an ordered list of prime integers
+    whose product equals to N.
+    >>> factorize(1)
+    [1]
+    >>> factorize(6)
+    [1, 2, 3]
+    >>> factorize(12)
+    [1, 2, 2, 3]
+    >>> factorize(0)
+    Traceback (most recent call last):
+        ...
+    ValueError: n cannot be 0
+    >>> factorize(-6)
+    [-1, 2, 3]
+    """
+    # 0 cannot be prime factorized
+    if n == 0:
+        raise ValueError("n cannot be 0")
+    # 1 is a prime factor for all positive ints, -1 is a prime factor for negative ints
+    factors = [1 if n > 0 else -1]
+    # Convert to positive integer to calculate prime factors if n is negative
+    if n < 0:
+        n = n * -1
+
     while (n % 2 == 0):
         factors.append(2)
         n = n/2
@@ -16,3 +39,7 @@ def factorize(n):
         factors.append(n)
 
     return factors
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
