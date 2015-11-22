1d920f70fb29c2743618d58cfbc03ee3a0005a62
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2015-11-22 15:41:04 -0500

    Finish a2q1

---
+++ asst2/profile.py
@@ -0,0 +1,25 @@
+PROFILE_FUNCTIONS = True
+PROFILE_RESULTS = {}
+
+def profile(func):
+    def profiler(*args, **kwargs):
+        if not PROFILE_FUNCTIONS:
+            func(*args, **kwargs)
+        else:
+            from time import clock
+            start = clock()
+            func(*args, **kwargs)
+            time_taken = clock() - start
+            if func.__name__ not in PROFILE_RESULTS:
+                PROFILE_RESULTS[func.__name__] = {
+                    "avg_time": time_taken,
+                    "num_times_called": 1
+                }
+            else:
+                old_result = PROFILE_RESULTS[func.__name__]
+                old_avg = old_result["avg_time"] * old_result["num_times_called"]
+                new_avg = (old_avg + time_taken) / (old_result["num_times_called"]+1)
+                PROFILE_RESULTS[func.__name__]["avg_time"] = new_avg
+                PROFILE_RESULTS[func.__name__]["num_times_called"] += 1
+
+    return profiler
